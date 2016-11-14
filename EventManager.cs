ef19a37f51b3d80a0c2a88805dd5289626af8699
Potapy4 <dev.potapy4@hotmail.com>
2016-11-14 15:10:49 +0200

    Implemented methods

--- EventManager.cs
+++ EventManager.cs
@@ -7,20 +7,32 @@ using System.Threading.Tasks;
 namespace PersonalScheduler
 {
 	public class EventManager
-	{		
-		public void ProcessEvents()
+	{
+        private List<ScheduledEvent> _events = new List<ScheduledEvent>();        
+
+        public void ProcessEvents()
 		{
 			
 		}
 
 		public void AddEvent(ScheduledEvent ev)
 		{
-			
+            IEnumerable<ScheduledEvent> res = _events.Where(x => x.Name == ev.Name);
+            if (res.Count() > 0)
+            {
+                throw new ArgumentException("Name can be unique");
+            }
+
+            _events.Add(ev);
+            _events.OrderBy(x => x.DateTime);
 		}
 
 		public void RemoveEvent(ScheduledEvent ev)
 		{
-
+            if (ev != null)
+            {
+                _events.Remove(ev);
+            }
 		}
 	}
 }
